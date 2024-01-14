0b1f90fdea10b8e2b68daab74a59e98c1b0647b6
Ice <94872146+Ice117@users.noreply.github.com>
2024-01-13 23:52:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import time
 email = "testemail@gmail.com"
 password = "iamtestingthiscode"
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+
+table = pd.read_csv("produtos.csv")
+
 pyautogui.PAUSE = 0.5
 
 pyautogui.press("win")
@@ -24,4 +27,31 @@ pyautogui.press("enter")
 
 time.sleep(3)
 
-pyautogui.press("tab")
+for i in table.index:
+    pyautogui.press("tab")
+
+    pyautogui.write(str(table.loc[i, "codigo"]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(table.loc[i, "marca"]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(table.loc[i, "tipo"]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(table.loc[i, "categoria"]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(table.loc[i, "preco_unitario"]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(table.loc[i, "custo"]))
+    pyautogui.press("tab")
+
+    obs = table.loc[i, "obs"]
+    if not pd.isna():
+        pyautogui.write(obs)
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(500)
+
